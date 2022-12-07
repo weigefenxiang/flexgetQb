ceac4d6def07824863627f8ab628d27b4377fc75
weigefenxiang <119787190+weigefenxiang@users.noreply.github.com>
2022-12-07 23:52:48 +0800

    Update qbstart.sh

--- docker/qbstart.sh
+++ docker/qbstart.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 flexget -c /root/config.yml --logfile  /root/logs/flexget.log  --cron  execute
-tail -n 40 /root/logs/flexget.log
-tail -n 50 /config/qBittorrent/logs/qbittorrent.log
-find / -name qbittorrent.log
+tail -n 20 /root/logs/flexget.log
 start=$(date +%M)
 
 sleep 1m
+tail -n 50 /config/qBittorrent/logs/qbittorrent.log
 i=1
 y=1
 while true
 do
   echo "正在删除种子..." 
   autoremove-torrents --conf=/root/autoremove.config.yml  --log=/root/logs/ &>/dev/null
-  if [[ $i -ge 4 ]];then   # 4 4分钟
+  if [[ $i -ge 4 ]];then   # 4 4分钟 80次  3分钟110
     echo "第 $i 次 开始执行flexget任务..." 
     flexget -c /root/config.yml --logfile  /root/logs/flexget.log  --cron  execute 
-    tail -n 40 /root/logs/flexget.log
+    tail -n 20 /root/logs/flexget.log
     tail -n 50 /config/qBittorrent/logs/qbittorrent.log
     tail -n 4 /root/logs/autoremove*.log
     i=0
