27e6d8905a8a7a4c59015c5a4f341bbfde02cd5e
weigefenxiang <119787190+weigefenxiang@users.noreply.github.com>
2022-12-07 21:38:15 +0800

    Update qbstart.sh

--- docker/qbstart.sh
+++ docker/qbstart.sh
@@ -14,6 +14,7 @@ do
   if [[ $i -ge 3 ]];then
     echo "第 $i 次 开始执行任务..." 
     flexget -c /root/config.yml --logfile  /root/logs/flexget.log  --test  execute
+    tail -n 20 /root/logs/flexget.log
     i=0
     let y++
     end=$(date +%M)
