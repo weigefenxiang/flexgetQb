2d704393615629b9da39981707976eb5efe779b3
weigefenxiang <119787190+weigefenxiang@users.noreply.github.com>
2022-12-07 21:33:22 +0800

    Update qbstart.sh

--- docker/qbstart.sh
+++ docker/qbstart.sh
@@ -11,21 +11,21 @@ while true
 do
   echo "正在删除种子..." 
   autoremove-torrents --conf=/root/autoremove.config.yml  --log=/root/logs/
-  if [ i -ge 3 ];then
+  if [[ $i -ge 3 ]];then
     echo "第 $i 次 开始执行任务..." 
     flexget -c /root/config.yml --logfile  /root/logs/flexget.log  --test  execute
     i=0
-    y+=1
+    let y++
     end=$(date +%M)
     take=$(( end - start ))
     echo "已经运行 ${take} 分钟。"
     cat /root/logs/flexget.log
-    if [ y -ge 3 ];then
+    if [[ $y -ge 3 ]];then
       echo "超时，正在退出！"
       break
     fi
   fi
-  i+=1
+  let i++
   echo "正在睡眠..." 
   sleep 1m
 done
